f0b2c6af5e5f885d3bc852ad88bd10113bc56059
Igor <igor.ushakov.10@gmail.com>
2019-04-24 19:53:24 +0300

    Касса (не полная)

--- UP_02.01/KassaForm.cs
+++ UP_02.01/KassaForm.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,6 +14,9 @@ namespace UP_02._01
 {
     public partial class KassaForm : Form
     {
+        DataBaseTables tables = new DataBaseTables();
+        DynamicObjects dynamicClass = new DynamicObjects();
+        
         public KassaForm()
         {
             InitializeComponent();
@@ -20,21 +24,67 @@ namespace UP_02._01
 
         private void KassaForm_Load(object sender, EventArgs e)
         {
+            dynamicClass.aggregateKassaForm = this;
+            dynamicClass.KassaFormFill();
+            Thread thread = new Thread(dgvSelecionFill);
+            thread.Start();
+
             DynamicObjects classDynamicObjects = new DynamicObjects();
             classDynamicObjects.aggregateKassaForm = this;
             classDynamicObjects.KassaFormFill();
         }
 
+        public void dgvSelecionFill()
+        {
+            Action action = () =>
+            {
+                try
+                {
+                    DataBaseTables data = new DataBaseTables();
+                    
+                    //filterDepartment = data.qrDepartment;
+                    tables.dtMedicamentiFill();
+                    tables.dependency.OnChange += onchangeApplication;
+                    dgvSelection.DataSource = tables.dtMedicamenti;
+                    dgvSelection.Columns[0].Visible = true;
+                    dgvSelection.Columns[1].HeaderText = "Количество выданных медикаментов";
+                    dgvSelection.Columns[2].Visible = false;
+                    dgvSelection.Columns[3].Visible = false;
+                    dgvSelection.Columns[4].HeaderText = "Номер чека";
+                    dgvSelection.Columns[5].HeaderText = "Название медикаментов";
+                    
+                    dgvSelection.Columns[8].Visible = false;
+                    dgvSelection.Columns[9].Visible = false;
+                    dgvSelection.Columns[10].Visible = false;
+                    dgvSelection.Columns[11].Visible = false;
+                    dgvSelection.Columns[12].Visible = false;
+                    dgvSelection.Columns[13].Visible = false;
+                    dgvSelection.Columns[14].Visible = false;
+                }
+                catch
+                {
+
+                }
+            };
+            Invoke(action);
+        }
+
+        private void onchangeApplication(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Info != SqlNotificationInfo.Invalid)
+                dgvSelecionFill();
+        }
+
         private void btWordCheck_Click(object sender, EventArgs e)
         {
-            Registry_Class.DirPath = @"C:\Users\1\Desktop\МПТ\Практика\Щаников";
-            switch (Registry_Class.DirPath == "Empty") //|| Registry_Class.OrganizationName == "Empty"
-                                                      //|| Registry_Class.DocBM == 0.0 || Registry_Class.DocTM == 0.0 ||
-                                                      //Registry_Class.DocRM == 0.0 || Registry_Class.DocLM == 0.0)
+            
+            switch (Registry_Class.DirPath == "Empty"|| Registry_Class.OrganizationName == "Empty"
+                                                      || Registry_Class.DocBM == 0.0 || Registry_Class.DocTM == 0.0 ||
+                                                      Registry_Class.DocRM == 0.0 || Registry_Class.DocLM == 0.0)
             {
                 case (true):
-                    //ApplicationConfigurationForm configurationForm = new ApplicationConfigurationForm();
-                    //configurationForm.ShowDialog();
+                    AppConfigForm configurationForm = new AppConfigForm();
+                    configurationForm.ShowDialog();
                     break;
                 case (false):
                     btCheckWord.Enabled = false;
